f4993dca6631213e6a063cb6a6d9c3d10f717dac
Mohammad Bilal <mohdbilalansari9100@gmail.com>
2025-09-23 10:45:11 +0530

    Time: 1 ms (84.54%), Space: 40.8 MB (70.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,35 @@
-class Solution {
+// class Solution {
+//     public int mySqrt(int x) {
+        // if (x < 2) return x;
+
+        // for (int i = 1; i <= x / 2+1; i++) {
+        //     long sq = (long) i * i;
+        //     if (sq == x) return i;
+        //     else if (sq > x) return i - 1;
+        // }
+        // return 1; // truly unreachable for x >= 2
+
+        // optimised solution 
+        class Solution {
     public int mySqrt(int x) {
         if (x < 2) return x;
 
-        for (int i = 1; i <= x / 2+1; i++) {
-            long sq = (long) i * i;
-            if (sq == x) return i;
-            else if (sq > x) return i - 1;
+        int left = 1, right = x / 2, ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long sq = (long) mid * mid;   // use long to avoid overflow
+            if (sq == x) return mid;
+            if (sq < x) {
+                ans = mid;      // mid is a candidate
+                left = mid + 1; // search right side
+            } else {
+                right = mid - 1; // search left side
+            }
         }
-        return 1; // truly unreachable for x >= 2
+        return ans; // floor(sqrt(x))
     }
 }
+
+
+    
+
