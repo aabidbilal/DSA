bfaefa9e320f35713cafbb6f14656b0aa2d95ac3
Mohammad Bilal <mohdbilalansari9100@gmail.com>
2025-10-01 00:33:06 +0530

    Time: 3 ms (58.03%), Space: 50.7 MB (9.12%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -11,6 +11,23 @@ class Solution {
             }
         }
         return max;
+
         
     }
 }
+//  int mo = 0;   // current streak
+//         int count = 0; // max streak
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i] == 0){
+//                 if(mo > count){
+//                     count = mo;
+//                 }
+//                 mo = 0; // reset streak
+//             } else {
+//                 mo++;
+//             }
+//         }
+//         // final check in case array ends with 1's
+//         count = Math.max(count, mo);
+
+//         System.out.println(count);
