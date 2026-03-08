9b874ddf9029074fcff8bf79eaf739e42e48d7be
Mohammad Bilal <mohdbilalansari9100@gmail.com>
2026-03-08 16:30:44 +0530

    Time: 1 ms (56.41%), Space: 43.6 MB (16.71%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -7,17 +7,22 @@ class Solution {
             boolean ispos = nums[i] > 0;
             set.add(i);
             int curr = i;
+            if (nums[curr] == 0) {
+                continue;
+            }
             //  2, -1, 1, 2, 2
 
             while (true) {
+
                 int next = calnext(nums, curr);
                 if (ispos) {
                     if (nums[next] < 0) {
                         break;
                     }
-                }else{
-                if (nums[next] >0) {
-                    break;}
+                } else {
+                    if (nums[next] > 0) {
+                        break;
+                    }
                 }
                 if (curr == next)
                     break;
@@ -28,13 +33,26 @@ class Solution {
                 set.add(next);
                 curr = next;
             }
-            // while()
+            curr=i;
+            if (ispos) {
+                while (nums[curr] > 0) {
+                    int next = calnext(nums, curr);
+                    nums[curr] = 0;
+                    curr = next;
+                }
+            } else {
+                while (nums[curr] < 0) {
+                    int next = calnext(nums, curr);
+                    nums[curr] = 0;
+                    curr = next;
+                }
+            }
         }
         return false;
     }
 
     public int calnext(int[] arr, int curr) {
         int n = arr.length;
-       return ((curr + arr[curr]) % n + n) % n;
+        return ((curr + arr[curr]) % n + n) % n;
     }
 }
