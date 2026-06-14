a0c5b7ecc39a98519c2d378eaf8970f10a495d12
Mohammad Bilal <mohdbilalansari9100@gmail.com>
2026-06-14 12:07:19 +0530

    Time: 2 ms (82.86%), Space: 42.8 MB (31.73%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.java
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -1,42 +1,52 @@
+class CharacterCount{
+    int count;
+    char c;
+    CharacterCount(int _count, char _c){
+        count = _count;
+        c = _c;
+    }
+}
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
-        int c_a = 0,c_b = 0,c_c = 0;
+        PriorityQueue<CharacterCount> maxHeap = new PriorityQueue<CharacterCount>((x, y) -> y.count - x.count);
+        
+        if ( a > 0 ){
+            maxHeap.add(new CharacterCount(a, 'a'));
+        }
+        if ( b > 0 ){
+            maxHeap.add(new CharacterCount(b, 'b'));
+        }
+        if ( c > 0 ){
+            maxHeap.add(new CharacterCount(c, 'c'));
+        }
         StringBuilder sb = new StringBuilder();
-        while (true){
-            if(a > 0 && (
-                (a >= b && a >= c && c_a < 2)||
-                (c_b == 2 && (a >= c && a <= b))||
-                (c_c == 2 && (a >= b && a <= c))
-            )){
-                sb.append('a');
-                c_a += 1;
-                c_b = 0;
-                c_c = 0;
-                a = a - 1;
 
-            }else if(b > 0 && (
-                (b >= a && b >= c && c_b < 2)||
-                (c_a == 2 && (b >= c && b <= a))||
-                (c_c == 2 && (b >= a && b <= c))
-            )){
-                sb.append('b');
-                c_b += 1;
-                c_a = 0;
-                c_c = 0;
-                b = b - 1;
-            }else if(c > 0 && (
-                (c >= b && c >= a && c_c < 2)||
-                (c_b == 2 && (c >= a && c <= b))||
-                (c_a == 2 && (c >= b && c <= a))
-            )){
-                sb.append('c');
-                c_c += 1;
-                c_b = 0;
-                c_a = 0;
-                c = c - 1;
+        while(!maxHeap.isEmpty()){
+            CharacterCount maxEle = maxHeap.poll();
+            int count = maxEle.count;
+            char _c = maxEle.c;
+
+            if(sb.length() >= 2 && sb.charAt(sb.length() - 1) == _c && sb.charAt(sb.length() - 2) == _c ){
+                if(maxHeap.isEmpty()){
+                    break;
+                }
+                CharacterCount secMaxEle = maxHeap.poll();
+                int sCount = secMaxEle.count;
+                char sC = secMaxEle.c;
+
+                sb.append(sC);
+                sCount -= 1;
+                if(sCount > 0){
+                    maxHeap.add( new CharacterCount(sCount, sC));
+                }
             }else{
-                break;
+                sb.append(_c);
+                count = count - 1;
             }
+            if ( count > 0 ){
+                    maxHeap.add( new CharacterCount(count, _c));
+                }
+
         }
         return sb.toString();
     }
