659deae916c10771975ca423535439d0045d3745
Mohammad Bilal <mohdbilalansari9100@gmail.com>
2026-08-06 23:09:23 +0530

    Time: 1 ms (61.59%), Space: 47.1 MB (58.73%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,38 +1,46 @@
 class Solution {
-    static boolean ans;
-    public void bfs(int i, int[][] adj, int[] visited){
-        visited[i] = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(q.size() > 0){
-            int front = q.remove();
-            int color = visited[front];
+    class Pair {
+        int node;
+        int color;
 
-            for(int ele : adj[front]){
-                if(visited[ele] == visited[front]){
-                    ans = false;
-                    return;
-                }
-                if(visited[ele] == -1){
-                    q.add(ele);
-                    visited[ele] = 1 - color;
-                }
-            }
+        Pair(int node, int color) {
+            this.node = node;
+            this.color = color;
         }
     }
 
-    public boolean isBipartite(int[][] adj) {
-      int n = adj.length;
-      ans = true;
-      int [] visited = new int[n];
-      Arrays.fill(visited, -1);
-    
-      for(int i = 0; i < n; i = i + 1){
-        if(ans == false)return false;
-        if( visited[i] == -1){
-            bfs(i, adj, visited);
+    public boolean bfs(int i, int[][] adj, int[] vis) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, 0));
+        vis[i] = 0;
+        while (!q.isEmpty()) {
+            int node = q.peek().node;
+            int color = q.peek().color;
+            q.remove();
+
+            for (int ele : adj[node]) {
+                if (vis[ele] == -1) {
+                    q.add(new Pair(ele, 1 - color));
+                    vis[ele] = 1 - color;
+                } else if (vis[ele] == color) {
+                    return false;
+                }
+            }
         }
-      }
-    return ans;
+        return true;
+    }
+
+    public boolean isBipartite(int[][] adj) {
+        int n = adj.length;
+        int[] vis = new int[n];
+        Arrays.fill(vis, -1);
+
+        for (int i = 0; i < n; i = i + 1) {
+
+            if (vis[i] == -1) {
+                if(!bfs(i, adj, vis))return false;
+            }
+        }
+        return true;
     }
 }
